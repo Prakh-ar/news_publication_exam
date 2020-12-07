6eaa81f0f9a64b96c4264009d71b3016973ebd84
unknown <prakhar7799@gmail.com>
2020-12-07 13:23:30 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { InputComponent } from './input/input.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DatabaseComponent } from './database/database.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    DatabaseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
